3fde847c86945700760c2872ae90038605f09020
Kenneth Ros <zhaho.ros@gmail.com>
2022-12-12 15:12:30 +0100

    Initial Commit

---
+++ app/src/main/java/gathering/example/gathering/MainActivity.java
@@ -0,0 +1,39 @@
+package gathering.example.gathering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myloginapp.R;
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this,"Login Successful!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+
+}
